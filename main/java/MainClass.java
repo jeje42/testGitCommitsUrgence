2b2963e31158acd77bb637370008bf1d1225fb5c
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-22 14:27:46 +0200

    urgence

--- main/java/MainClass.java
+++ main/java/MainClass.java
@@ -5,6 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(travail normal);
+		
+		System.out.println(urgence);
 
 	}
 
