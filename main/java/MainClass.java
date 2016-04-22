9a5b33834d5d3bf3b7d04857261599f0a0812a69
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-22 14:10:17 +0200

    travail normal2

--- main/java/MainClass.java
+++ main/java/MainClass.java
@@ -5,7 +5,9 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(travail normal1);
+		System.out.println("travail normal1");
+		
+		System.out.println("travail normal2");
 	}
 
 }
