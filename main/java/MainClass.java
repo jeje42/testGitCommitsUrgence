ecc204477c308097993796ba81037ab5ce5b2516
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-22 14:08:16 +0200

    reset

--- main/java/MainClass.java
+++ main/java/MainClass.java
@@ -5,18 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println("Commit 1");
-		
-		System.out.println("Commit 2");
-		
-		System.out.println("Commit 3");
-		
-		System.out.println("Travail normal 1");
-		
-		System.out.println("Urgence !");
-		
-		System.out.println("Travail Normal 2");
-
 	}
 
 }
