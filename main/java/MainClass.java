401344a40cd2b76a4bba7fab620645d672140bdf
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-25 15:46:26 +0200

    Travail de fond - tâche 1

--- main/java/MainClass.java
+++ main/java/MainClass.java
@@ -5,6 +5,8 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Etat initial");
+		
+		System.out.println("Travail de fond - tâche 1");
 
 	}
 
