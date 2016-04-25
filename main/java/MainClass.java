a955c87b13e546d03dad6e8415b9252fcc7ea0ea
jeje42 <jerome.guidon@insa-lyon.fr>
2016-04-25 16:13:32 +0200

    Etat initial - reset

--- main/java/MainClass.java
+++ main/java/MainClass.java
@@ -5,9 +5,6 @@ public class MainClass {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Etat initial");
-		
-		System.out.println("Travail de fond - tâche 1");
-
 	}
 
 }
